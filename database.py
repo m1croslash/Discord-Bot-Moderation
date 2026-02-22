a49f3935277974497a73bb0f1e5fc6528c62e2e3
microslash <elinalazarevamilana@gmail.com>
2026-02-22 23:55:51 +0900

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 import sqlite3
 
 # Connect to the database
-conn = sqlite3.connect('bot_database.db')
-cursor = conn.cursor()
+con = sqlite3.connect('bot_database.db')
+cursor = con.cursor()
 
 # Function to initialize the database (create tables if they don't exist)
 def initialize(): 
@@ -15,14 +15,14 @@ def initialize():
                 reason TEXT
             )
         ''')
-        conn.commit()
+        con.commit()
     except sqlite3.OperationalError:
         print("Table warnings already exists...")
 
 # Function to add a warning
 def add_warning(user_id, guild_id, reason):
     cursor.execute('INSERT INTO warnings (user_id, guild_id, reason) VALUES (?, ?, ?)', (user_id, guild_id, reason))
-    conn.commit()
+    con.commit()
 
 # Function to get all warnings for a specific user
 def get_warnings(user_id, guild_id):
@@ -32,8 +32,8 @@ def get_warnings(user_id, guild_id):
 # Function to clear all warnings for a specific user
 def clear_warnings(user_id, guild_id):
     cursor.execute('DELETE FROM warnings WHERE user_id = ? AND guild_id = ?', (user_id, guild_id))
-    conn.commit()
+    con.commit()
 
 # Function to close the database connection
 def close_connection():
-    conn.close()
+    con.close()
